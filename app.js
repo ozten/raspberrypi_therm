704a9cd3777e5673997ddade4d42913874bc79f2
Austin King <shout@ozten.com>
2014-05-24 14:56:28 -0700

    Add CORS support

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
+var http = require('http');
+
 var therm = require('./w1_therm');
-therm(function(err, temps) {
-  console.log('finally', err, temps);
+
+var hostname = process.env.W1_HOSTNAME || '0.0.0.0';
+var port = process.env.W1_PORT || '8000';
+var canned = !! process.env.CANNED_DATA ? true : false;
+
+console.log('Canned data?', canned);
+
+var server = http.createServer(function(req, res) {
+  if (canned) {
+    var cels = 40 + (Math.random() * 10) - 5;
+    var far = (cels * (9 / 5)) + 32;
+    return sendJson(res, [{
+      id: 'canned',
+      c: cels,
+      f: far
+    }]);
+  } else {
+    therm(function(err, temps) {
+      if (err) {
+        console.log(err);
+        return sendJson(res, {
+          err: err
+        }, 500);
+      }
+      return sendJson(res, temps);
+    });
+  }
 });
+
+server.listen(port, hostname, function() {
+  console.log('W1 Thermometer available at http://' + hostname + ':' + port);
+});
+
+function sendJson(res, json, statusCode) {
+  res.statusCode = statusCode || 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.end(JSON.stringify(json));
+}
--- w1_therm.js
+++ w1_therm.js
@@ -14,41 +14,39 @@ module.exports = function reading(cb) {
         devices.push(file);
       }
     });
-    async.each(devices, function(file, filesCb) {
-      console.log('reading', path.join(devicesDir, file, 'w1_slave'));
-      fs.readFile(path.join(devicesDir, file, 'w1_slave'), {
-          encoding: 'utf8'
-        },
-        function(err, data) {
-          if (err) {
-            console.error(err);
-            filesCb(err);
-          } else {
-            var lines = data.split('\n');
-            if (lines[0].indexOf('YES') !== -1) {
-              var parts = lines[1].split('=');
-              var cels = parseInt(parts[1], 10) / 1000;
-              var far = (cels * (9 / 5)) + 32;
-              console.log(far, 'F');
-              filesCb(null, {
-                id: file,
-                c: cels,
-                f: far
-              });
+    if (0 === devices.length) return cb(new Error('no w1'));
+    async.map(devices, function(file, filesCb) {
+        fs.readFile(path.join(devicesDir, file, 'w1_slave'), {
+            encoding: 'utf8'
+          },
+          function(err, data) {
+            if (err) {
+              filesCb(err);
             } else {
-              console.log('garbage');
-              filesCb(null, null);
+              var lines = data.split('\n');
+              if (lines[0].indexOf('YES') !== -1) {
+                var parts = lines[1].split('=');
+                var cels = parseInt(parts[1], 10) / 1000;
+                var far = (cels * (9 / 5)) + 32;
+                console.log(file, cels, far);
+                filesCb(null, {
+                  id: file,
+                  c: cels,
+                  f: far
+                });
+              } else {
+                console.log('No reading');
+                filesCb(null, []);
+              }
             }
-          }
-        });
-    });
-  }, function(err) {
-console.log('and lastly');
-    if (err, temps) {
-      cb(err);
-    } else {
-      cb(null, temps);
-    }
+          });
+      },
+      function(err, temps) {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, temps);
+        }
+      });
   });
-
-}
+};
